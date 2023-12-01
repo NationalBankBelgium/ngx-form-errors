0962c7d3cf2a9f8ffe240e26262a5dce67036ce5
Michael HENKENS <Michael.HENKENS@nbb.be>
2023-12-01 11:58:55 +0100

    chore(angular): upgrade to angular 12
    Upgrade version to angular 12

--- base.spec.ts
+++ base.spec.ts
@@ -5,7 +5,7 @@ import "core-js/es6";
 import "core-js/es7/reflect";
 import "core-js/stage/4";
 
-import "zone.js/dist/zone";
+import "zone.js";
 import "zone.js/dist/long-stack-trace-zone";
 import "zone.js/dist/proxy"; // since zone.js 0.6.15
 import "zone.js/dist/sync-test";
